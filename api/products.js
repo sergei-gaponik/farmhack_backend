b3e907da996bd1b93ef1cbbd4c831660ec8a2ed8
Sergei <sergei.gaponik@gmail.com>
2020-11-21 09:23:03 +0100

    srxydftghcbv

--- api/products.js
+++ api/products.js
@@ -36,9 +36,9 @@ const getProducts = async (req, res) => {
 
     const { hubID } = details
 
-    console.log(hubID)
+    console.log(hubID,  req.custom.username)
 
-    const distance = await geo.getDistanceFromHubToUser(hubID, req.custom.username)
+    const distance = 30//await geo.getDistanceFromHubToUser(hubID, req.custom.username)
 
     if(distance < process.global.maxDistanceFromHub){
 
